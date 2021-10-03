bd2373926ddd68443df93bc7ccb03b95bb164cfd
ipegte93 <ipegte93@gmail.com>
2021-10-03 16:38:14 +0900

    하트비트 테스트 완료

--- test/하트비트.py
+++ test/하트비트.py
@@ -16,9 +16,21 @@ async def main():
     async with websockets.connect("wss://gateway.discord.gg") as websocket:
         res = await websocket.recv()
         res = json.loads(res)
-
         heartbeat_interval = res["d"]["heartbeat_interval"]
-        data = await heartbeat(heartbeat_interval)
-        await websocket.send(data)
+        print(int(heartbeat_interval)/1000)
+
+        async def heart():
+            data = {
+                "op": 1,
+                "d": None
+            }
+            data = json.dumps(data)
+
+            await asyncio.sleep(int(heartbeat_interval)/1000)
+            print("send")
+            await websocket.send(data)
+            await asyncio.create_task(heart())
+
+        await heart()
 
 asyncio.run(main())
