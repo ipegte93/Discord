e81bf0ee1d99f3839dfd0bfe16e480207d34e226
ipegte93 <ipegte93@gmail.com>
2021-10-03 16:26:23 +0900

    하트비트 한번 성공

--- test/하트비트.py
+++ test/하트비트.py
@@ -1,9 +1,24 @@
 import asyncio
 import websockets
+import json
+
+async def heartbeat(interval):
+
+    data = {
+    "op": 1,
+    "d" : None
+    }
+    print(interval)
+    await asyncio.sleep(int(interval)/1000)
+    return json.dumps(data)
 
 async def main():
     async with websockets.connect("wss://gateway.discord.gg") as websocket:
-        data = await websocket.recv()
-        print(data)
+        res = await websocket.recv()
+        res = json.loads(res)
+
+        heartbeat_interval = res["d"]["heartbeat_interval"]
+        data = await heartbeat(heartbeat_interval)
+        await websocket.send(data)
 
 asyncio.run(main())
