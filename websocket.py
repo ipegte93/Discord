aaa739aeedc3f2d21185cc7a6bcb97536b6d25bc
ipegte93 <ipegte93@gmail.com>
2021-10-03 20:16:26 +0900

    정리

--- websocket.py
+++ websocket.py
@@ -15,42 +15,6 @@ class Websocket:
         async with websockets.connect("wss://gateway.discord.gg") as self.ws:
             await self.handler()
 
-    async def handler(self):
-        await self.identify()
-        while True:
-            msg = await self.ws.recv()
-            msg = json.loads(msg)
-            await self.consumer(msg)
-
-    async def consumer(self, msg):
-        op = msg["op"]
-
-        if op == 0: # Dispatch
-            print(msg["d"])
-        # elif op == 7: # Reconnect
-        #     print(msg)
-        # elif op == 9: # Invalid Session
-        #     print(msg)
-        elif op == 10: # Hello
-            self.interval = int(msg["d"]["heartbeat_interval"])/1000
-            asyncio.create_task(self.heartbeat())
-        elif op == 11: #Heartbeat ACK
-            asyncio.create_task(self.heartbeat())
-        # else:
-        #     print(msg)
-
-    async def heartbeat(self):
-        await asyncio.sleep(self.interval)
-
-        data = {
-            "op": 1,
-            "d": None
-        }
-        data = json.dumps(data)
-
-        print("send Heartbeat")
-        await self.ws.send(data)
-
     async def identify(self):
         data = {
             "op": 2,
@@ -69,6 +33,38 @@ class Websocket:
         print("identifying")
         await self.ws.send(data)
 
-if __name__ == "__main__":
-    test = Websocket()
-    asyncio.run(test.connect())
+    async def handler(self):
+        await self.identify()
+        while True:
+            msg = await self.ws.recv()
+            msg = json.loads(msg)
+            await self.consumer(msg)
+
+    async def consumer(self, msg):
+        op = msg["op"]
+
+        if op == 0: # Dispatch
+            print(msg["d"])
+        elif op == 7: # Reconnect
+            print(msg)
+        elif op == 9: # Invalid Session
+            print(msg)
+        elif op == 10: # Hello
+            self.interval = int(msg["d"]["heartbeat_interval"])/1000
+            asyncio.create_task(self.heartbeat())
+        elif op == 11: #Heartbeat ACK
+            asyncio.create_task(self.heartbeat())
+        else:
+            print(msg)
+
+    async def heartbeat(self):
+        await asyncio.sleep(self.interval)
+
+        data = {
+            "op": 1,
+            "d": None
+        }
+        data = json.dumps(data)
+
+        print("send Heartbeat")
+        await self.ws.send(data)
