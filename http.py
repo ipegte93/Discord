0bd35cf8479d4a92c46e06c76fbd27594c12ed9d
ipegte93 <ipegte93@gmail.com>
2021-10-05 02:38:46 +0900

    update

--- http.py
+++ http.py
@@ -1,5 +1,19 @@
-import requests
-import json
+import aiohttp
+import asyncio
 
+class rest_api:
+    def __init__(self, BOT_TOKEN):
+        self.token = BOT_TOKEN
 
-# REST API로 구현
+    def send_message(self ,CHANNEL_ID, msg):
+        payload = {
+            "content": msg,
+            "tts": False,
+        }
+        asyncio.run(self.post(payload))
+
+    async def post(self, payload):
+        async with aiohttp.ClientSession() as session:
+            header = {"authorization": "Bot " + self.token}
+            async with session.post("https://discord.com/api/v9/channels/862348023764746279/messages", data=payload, headers=header) as res:
+                print(await res.text())
