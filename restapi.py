3b2d8d86d3131743a1cf41df9e6fdc543050eaf6
ipegte93 <ipegte93@gmail.com>
2021-10-05 02:43:11 +0900

    rename

--- restapi.py
+++ restapi.py
@@ -1,19 +1,19 @@
 import aiohttp
 import asyncio
 
-class rest_api:
+class restapi:
     def __init__(self, BOT_TOKEN):
         self.token = BOT_TOKEN
 
-    def send_message(self ,CHANNEL_ID, msg):
+    def send_message(self, CHANNEL_ID, msg):
         payload = {
             "content": msg,
             "tts": False,
         }
         asyncio.run(self.post(payload))
 
-    async def post(self, payload):
+    async def post(self, CHANNEL_ID, payload):
         async with aiohttp.ClientSession() as session:
             header = {"authorization": "Bot " + self.token}
-            async with session.post("https://discord.com/api/v9/channels/862348023764746279/messages", data=payload, headers=header) as res:
+            async with session.post("https://discord.com/api/v9/channels/"+CHANNEL_ID+"/messages", data=payload, headers=header) as res:
                 print(await res.text())
