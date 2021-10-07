734df5a25d24b2baaaacfd60b1de33c42f1572c0
ipegte93 <ipegte93@gmail.com>
2021-10-08 03:29:43 +0900

    rename

---
+++ restapi.py
@@ -0,0 +1,19 @@
+import aiohttp
+
+class Route:
+    def __init__(self, method: str, path: str):
+        self.url = "https://discord.com/api/v9" + path
+        self.method = method
+
+class RestAPI:
+    def __init__(self, BOT_TOKEN: str):
+        self.headers = dict()
+        self.headers['authorization'] = "Bot " + BOT_TOKEN
+
+    async def request(self, route: Route, **kwargs: any):
+        payload = None
+        if "payload" in kwargs:
+            payload = kwargs["payload"]
+
+        async with aiohttp.request(route.method, route.url, headers=self.headers, data=payload) as response:
+            print(await response.text())
--- websocket.py
+++ websocket.py
@@ -62,7 +62,7 @@ class Websocket:
         if response['t'] == "READY":
             print("Bot ready")
         else:
-            ResponseHandler(response)
+            ResponseHandler(response, self.TOKEN)
 
     async def __heartbeat(self):
         await asyncio.sleep(self.heartbeat_interval)
@@ -74,21 +74,3 @@ class Websocket:
         payload = json.dumps(payload)
 
         await self.ws.send(payload)
-
-class Route:
-    def __init__(self, method: str, path: str):
-        self.url = "https://discord.com/api/v9" + path
-        self.method = method
-
-class RestAPI:
-    def __init__(self, BOT_TOKEN: str):
-        self.headers = dict()
-        self.headers['authorization'] = "Bot " + BOT_TOKEN
-
-    async def request(self, route: Route, **kwargs: any):
-        payload = None
-        if "payload" in kwargs:
-            payload = kwargs["payload"]
-
-        async with aiohttp.request(route.method, route.url, headers=self.headers, data=payload) as response:
-            print(await response.text())
