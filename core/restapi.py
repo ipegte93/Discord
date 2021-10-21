43e6c5a1b73d129758f22b08c5926cf775eb28bf
ipegte93 <ipegte93@gmail.com>
2021-10-22 01:21:36 +0900

    Done

--- core/restapi.py
+++ core/restapi.py
@@ -15,6 +15,6 @@ class RestAPI:
         payload = None
         if "payload" in kwargs:
             payload = kwargs["payload"]
-        payload = json.dumps(payload)
-        async with aiohttp.request(route.method, route.url, headers=self.headers, json=payload) as response:
+
+        async with aiohttp.request(route.method, route.url, headers=self.headers, data=payload) as response:
             print(await response.text())
